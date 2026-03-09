a7a15aa913e895eaa6e7d860fcc094e34ba73783
Mehdiakha <akhabbarmehdi@gmail.com>
2026-03-09 01:41:10 +0000

    commit

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,9 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+'''
+
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
@@ -17,3 +23,9 @@ nums = [2, 11, 7, 14]
 target = 18
 
 print(twoSum(nums, target))
+
+'''
+we track each number and index in our list, in each iteration we check of the needed number is in seen,
+if not we store the current number in it, if we see it, we return the seen number's index and the current number index as a list
+
+'''
